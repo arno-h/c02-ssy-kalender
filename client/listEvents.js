6c4cc5ab2fe9270121793fd63b54cf7925c34bc5
Arno Hollosi <arno.hollosi@campus02.at>
2017-12-21 15:37:36 +0100

    Implementieren des Clients mithilfe der erweiterten Query-Funktionen

--- client/listEvents.js
+++ client/listEvents.js
@@ -12,6 +12,24 @@ if (process.argv.length < 3) {
 let user = process.argv[2];
 
 
+async function listEvents(username) {
+    let response = await axios(hostUrl + '/users/?name=' + username);
+    const users = response.data;
+    if (users.length === 0) {
+        console.log('Keine Person mit diesem Namen gefunden.');
+        process.exit(0);
+    }
+    const user = users[0]; // wir nehmen nur erstes Element aus Array
+    response = await axios.get(hostUrl + '/events/?participant=' + user.$loki);
+    const events = response.data;
+    events.sort(compareEventTime);
+    for (let event of events) {
+        console.log(event.startTime + " - " + event.name + " @ " + event.place);
+    }
+}
+
+listEvents(user).then();
+
 // Hilfsfunktion zum Sortieren nach Uhrzeit
 // Array sortieren mit: array_variable.sort(compareEventTime)
 function compareEventTime(ev1, ev2) {
